0430410326b6f05be85d7acdeb5a67082890782a
Lucas Silvestre <lukas.silvestre@gmail.com>
2013-12-04 22:09:18 +0000

    Add a RESTful Web Services from Patterns

---
+++ HelloWorldApplication/src/java/helloWorld/HelloWorld.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloWorld;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * REST Web Service
+ *
+ * @author lsilvs
+ */
+@Path("helloworld")
+@RequestScoped
+public class HelloWorld {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HelloWorld
+     */
+    public HelloWorld() {
+    }
+
+    /**
+     * Retrieves representation of an instance of helloWorld.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/html")
+    public String getHtml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HelloWorld
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("text/html")
+    public void putHtml(String content) {
+    }
+}
